2531e23811059f91a566dafffbb2b10ec286b979
RajviDave <rajvidave22@gmail.com>
2026-07-17 15:27:17 +0530

    youtube transcript fetched

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import os
 from dotenv import load_dotenv
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI
 
-os.environ['OPENAI_API_KEY']=os.getenv("gemini_api_key")
+load_dotenv()
 
+google_api_key=os.getenv("gemini_api_key")
+
+video_id="LPZh9BOjkQs"
+
+try:
+    ytt_api=YouTubeTranscriptApi()
+    transcript=ytt_api.fetch(video_id,languages=["en"])
+    transcripts = " ".join(chunk.text for chunk in transcript)
+    print(transcript)
+
+except TranscriptsDisabled:
+    print("No transcript available for this video")
