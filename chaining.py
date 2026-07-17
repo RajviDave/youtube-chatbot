8d99c22008e34d31b2888c14ec29af1a76cc664d
RajviDave <rajvidave22@gmail.com>
2026-07-17 21:17:25 +0530

    chaining done

--- chaining.py
+++ chaining.py
@@ -1,2 +1,7 @@
 from langchain_core.runnables import RunnableParallel,RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
+
+def format_docs(retrieved_docs):
+    context_text="\n\n".join(doc.page_content for doc in retrieved_docs)
+    return context_text
+
