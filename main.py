6b5e7d78a1d809b9488ebaa4267aca38453ec69b
RajviDave <rajvidave22@gmail.com>
2026-07-17 15:54:55 +0530

    vector store

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI
+from langchain_community.vectorstores import FAISS
 
 load_dotenv()
 
@@ -12,9 +13,19 @@ video_id="LPZh9BOjkQs"
 
 try:
     ytt_api=YouTubeTranscriptApi()
-    transcript=ytt_api.fetch(video_id,languages=["en"])
-    transcripts = " ".join(chunk.text for chunk in transcript)
-    print(transcript)
+    transcript_list=ytt_api.fetch(video_id,languages=["en"])
+    transcript = " ".join(chunk.text for chunk in transcript_list)
+    #print(transcript)
 
 except TranscriptsDisabled:
     print("No transcript available for this video")
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=800,chunk_overlap=100)
+chunks=splitter.create_documents([transcript])
+
+#print(len(chunks))
+
+embeddings=GoogleGenerativeAIEmbeddings(model="gemini-embedding-2")
+vector_store=FAISS.from_documents(chunks,embeddings)
+
+print(vector_store.index_to_docstore_id)
